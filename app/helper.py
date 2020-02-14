ebd76dbb30a6f3555fafbf991cad076a83bf9709
rizal setya perdana <rizalespe@gmail.com>
2020-02-14 10:37:41 +0900

    adding helper class

---
+++ app/helper.py
@@ -0,0 +1,23 @@
+class Vocabulary(object):
+    """This class is a custom data structure object with the basic functional
+    such as adding new word, retrieving the index of word, and find the length
+    of the 
+    """
+    def __init__(self):
+        self.word2idx = {}
+        self.idx2word = {}
+        self.idx = 0
+
+    def add_word(self, word):
+        if not word in self.word2idx:
+            self.word2idx[word] = self.idx
+            self.idx2word[self.idx] = word
+            self.idx += 1
+
+    def __call__(self, word):
+        if not word in self.word2idx:
+            return self.word2idx['<unk>']
+        return self.word2idx[word]
+
+    def __len__(self):
+        return len(self.word2idx)
--- app/vocabulary.py
+++ app/vocabulary.py
@@ -1,6 +1,7 @@
 import nltk
 import pickle
 from collections import Counter
+from helper import Vocabulary
 
 # This code is a Python script that use for mapping from text into numerical 
 # index which specify for visual storytelling (VIST) task. The input of this 
@@ -16,30 +17,6 @@ from collections import Counter
 
 # Some part of this code is referenced to https://github.com/tkim-snu/GLACNet 
 # work. 
-
-class Vocabulary(object):
-    """This class is a custom data structure object with the basic functional
-    such as adding new word, retrieving the index of word, and find the length
-    of the 
-    """
-    def __init__(self):
-        self.word2idx = {}
-        self.idx2word = {}
-        self.idx = 0
-
-    def add_word(self, word):
-        if not word in self.word2idx:
-            self.word2idx[word] = self.idx
-            self.idx2word[self.idx] = word
-            self.idx += 1
-
-    def __call__(self, word):
-        if not word in self.word2idx:
-            return self.word2idx['<unk>']
-        return self.word2idx[word]
-
-    def __len__(self):
-        return len(self.word2idx)
 
 class GenerateVocabulary(object):
     # This class will be initialized by the dataset class if the vocabulary file is not exist
